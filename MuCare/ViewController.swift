2efcfe65b3046c5989e616ef10b978a1a90b0bdb
ATPL <atpl@atpls-macbook-pro.local>
2015-11-14 17:16:36 +0530

    Initial Commit

---
+++ MuCare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MuCare
+//
+//  Created by ATPL on 11/14/15.
+//  Copyright © 2015 ATPL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
